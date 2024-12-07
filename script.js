b791fe07f4bf887bd212e955cf7990a9d435ca3c
rohithzz <117429092+Rohithzz@users.noreply.github.com>
2024-12-07 16:34:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function printData() {
     const fullName = document.getElementById('fullName').value;
+    const usn = document.getElementById('usn').value;
+    const email = document.getElementById('email').value;
     const dob = document.getElementById('dob').value;
     const phoneNumber = document.getElementById('phoneNumber').value;
     const gender = document.getElementById('gender').value;
     const message = document.getElementById('message').value;
 
-    const data = `Full Name: ${fullName}\nDate of Birth: ${dob}\nPhone Number: ${phoneNumber}\nGender: ${gender}\nAdditional Message: ${message}`;
+    const data = `Full Name: ${fullName}\nUSN: ${usn}\nEmail: ${email}\nDate of Birth: ${dob}\nPhone Number: ${phoneNumber}\nGender: ${gender}\nAdditional Message: ${message}`;
 
     const element = document.createElement('a');
     const file = new Blob([data], {type: 'text/plain'});
@@ -19,13 +21,17 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     event.preventDefault();
 
     const fullName = document.getElementById('fullName').value;
+    const usn = document.getElementById('usn').value;
+    const email = document.getElementById('email').value;
     const dob = document.getElementById('dob').value;
     const phoneNumber = document.getElementById('phoneNumber').value;
     const gender = document.getElementById('gender').value;
     const message = document.getElementById('message').value;
 
-    // Display the data on the same page
+    // dispaly functining guru
     document.getElementById('fullNameDisplay').textContent = `Full Name: ${fullName}`;
+    document.getElementById('usnDisplay').textContent = `USN: ${usn}`;
+    document.getElementById('emailDisplay').textContent = `Email: ${email}`;
     document.getElementById('dobDisplay').textContent = `Date of Birth: ${dob}`;
     document.getElementById('phoneNumberDisplay').textContent = `Phone Number: ${phoneNumber}`;
     document.getElementById('genderDisplay').textContent = `Gender: ${gender}`;
